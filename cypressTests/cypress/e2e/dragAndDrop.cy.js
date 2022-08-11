44562c801e2b050352e384b9c1454d676ebddf3b
jlmooreiii <jloftonmooreiii@gmail.com>
2022-08-10 23:47:17 -0700

    needed to add functionality to check both columns

--- cypressTests/cypress/e2e/dragAndDrop.cy.js
+++ cypressTests/cypress/e2e/dragAndDrop.cy.js
@@ -4,10 +4,16 @@ describe('drag and drop test', () => {
     })
     it('can drag and drop', () => {
         // get inital location of column-b
+        let initialPositionA
         let initialPositionB
-        cy.get('div[id="column-a"]').then(
-            ($columnB) => {
-                initialPositionB = $columnB.position();
+        cy.get('div[id="column-a"]').contains('A').then(
+            ($textA) => {
+                initialPositionA = $textA.position();
+            });
+        
+        cy.get('div[id="column-b"]').contains('B').then(
+            ($textB) => {
+                initialPositionB = $textB.position();
             });
                 
         const dataTransfer = new DataTransfer();
@@ -21,11 +27,16 @@ describe('drag and drop test', () => {
         });
 
         // check to make sure that the new location of column-a
-        // is where the old location of column-b
-        cy.get('div[id="column-a"]').then(
-            ($columnA) => {
-                expect($columnA.position()).deep.equal(initialPositionB)
-            });
+        // is where the old location of column-b and vice versa
+        cy.get('div[id="column-b"]').contains('A').then(
+            ($textA) => {
+                expect($textA.position()).deep.equal(initialPositionB)
+        });
+
+        cy.get('div[id="column-a"]').contains('B').then(
+            ($textB) => {
+                expect($textB.position()).deep.equal(initialPositionA)
+        });
     })
 })
 
